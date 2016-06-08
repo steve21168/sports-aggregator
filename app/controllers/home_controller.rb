1035e817c148259a80583c019c13a86c169e33e4
steve21168 <steve21168@gmail.com>
2016-06-08 14:59:34 -0400

    added images and all league pages cycle

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,17 +1,7 @@
 class HomeController < ApplicationController
 
   def home
-    @articles = random_articles
-    @last_updated = Article.last.created_at
   end
 
-  private
-
-  def random_articles
-    articles = Article.all
-    article_array = []
-    10.times { article_array << articles.sample }
-    article_array
-  end
 
 end
