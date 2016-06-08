7323806eb265389f7a731605a8c85f7bb3154f88
steve21168 <steve21168@gmail.com>
2016-06-08 10:58:50 -0400

    added caraseoul to home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,17 @@
 class HomeController < ApplicationController
 
   def home
+    @articles = random_articles
+    @last_updated = Article.last.created_at
+  end
 
-  end 
+  private
+
+  def random_articles
+    articles = Article.all
+    article_array = []
+    10.times { article_array << articles.sample }
+    article_array
+  end
 
 end
